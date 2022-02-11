74fd6373a0268aa3085ea542494d67124f1518f6
kennjr <ken.juniour@student.moringaschool.com>
2022-02-11 23:03:37 +0300

    Added the access token to the environments file

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  access_token: "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"
 };
 
 /*
