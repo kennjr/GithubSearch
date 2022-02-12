0c7fc7bb1790f5d66a52411f19002629798527af
kennjr <ken.juniour@student.moringaschool.com>
2022-02-12 20:18:02 +0300

    Changed the type of some of the properties of the user model

--- src/app/models/User.ts
+++ src/app/models/User.ts
@@ -2,8 +2,8 @@ export class User{
     html_url!:string;
     avatar_url!:string;
     username!:string;
-    email!:string;
-    followers_count!:number;
-    following_count!:number;
-    public_repo_count!:number;
+    followers_url!:string;
+    following_url!:string;
+    repos_url!:string;
+    api_url!:string;
 }
