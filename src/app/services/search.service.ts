e4151e22e85f7dc87b1fde6ab744e4cca818d7f3
kennjr <ken.juniour@student.moringaschool.com>
2022-02-12 09:16:40 +0300

    Changed the search request url and changed how I access the data from the service

--- src/app/services/search.service.ts
+++ src/app/services/search.service.ts
@@ -1,6 +1,13 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
+import { BehaviorSubject } from 'rxjs';
+
+const httpOptions = {
+  headers : new HttpHeaders({
+    "Authorization": environment.access_token
+  })
+}
 
 @Injectable({
   providedIn: 'root'
@@ -9,15 +16,37 @@ export class SearchService {
 
   constructor(private httpClient: HttpClient) { }
 
+  searchType:string = "Users";
+  resultsList = new BehaviorSubject<any>([]);
+
   makeSearchRequest (searchstring :string){
     let url = this.createSearchUrl(searchstring)
-    
-    return this.httpClient.get(url);
+    if(url != ""){
+      this.httpClient.get(url, httpOptions).subscribe((response :any) => {
+        console.log("This is the data " + response.data)
+        this.resultsList.next(response.data);
+      })
+    }
   }
 
   createSearchUrl (searchstring :string): string{
     let trimmedSearchString = searchstring.trim()
-    let requestUrl = 'https://api.github.com/users/' + trimmedSearchString + '?access_token=' + environment.access_token;
-    return requestUrl;
+    let searchUrl = "";
+    if(this.searchType === "Users"){
+      searchUrl = `https://api.github.com/search/users?q=${trimmedSearchString}`;
+    }else if(this.searchType === "Repos"){
+      searchUrl = `https://api.github.com/search/repositories?q=${trimmedSearchString}`;
+    }
+    
+    return searchUrl;
   }
+
+  getSearchResults (){
+    return this.resultsList.asObservable()
+  }
+
+  updateSearchType(newType :string){
+    this.searchType = newType
+  }
+
 }
