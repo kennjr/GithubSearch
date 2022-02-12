19bf057794b78c035034f824a2392b16d63cc64d
kennjr <ken.juniour@student.moringaschool.com>
2022-02-12 09:59:35 +0300

    Created the user model and added some properties to it

---
+++ src/app/models/User.ts
@@ -0,0 +1,9 @@
+export class User{
+    html_url!:string;
+    avatar_url!:string;
+    username!:string;
+    email!:string;
+    followers_count!:number;
+    following_count!:number;
+    public_repo_count!:number;
+}
